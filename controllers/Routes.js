b134c0f3143c2648219603fa528c6eb5107d88e1
Himank17Gupta <hemankgupta@gmail.com>
2019-10-24 19:15:30 +0530

    added declarative routing comments

--- controllers/Routes.js
+++ controllers/Routes.js
@@ -1,12 +1,12 @@
 var express=require('express');
 var route=express.Router();
 
-route.get('/xyz',(req,res)=>{
+route.get('/xyz',(req,res)=>{       //testing url string
     console.log(req.body);
 res.send('welcome bitches again');
 });
 
-route.post('/signup',(req,res)=>{
+route.post('/signup',(req,res)=>{           // reqObject-->{"userid":"","password":"","email":"","contact":""}
     var reqObject=req.body;
     console.log('req body is : ',reqObject);
     var operations=require('../database/helpers/Operation');
@@ -14,7 +14,7 @@ route.post('/signup',(req,res)=>{
 });
  
 // /login
-route.post('/login',(req,res)=>{
+route.post('/login',(req,res)=>{            // reqObject -->{"userid":"","password":""}
     var reqObject=req.body;
    // console.log('req body is : ',reqObject);
     var operations=require('../database/helpers/Operation');
@@ -22,7 +22,7 @@ route.post('/login',(req,res)=>{
 });
 
 // /createpoll
-route.post('/createPoll',(req,res)=>{
+route.post('/createPoll',(req,res)=>{  //reqObject -->{question:"",user_Id:"",options:["option1","option2","option3","option4"],createdOn:"Date"}
     var reqObject=req.body;
     //console.log('req body is : ',reqObject);
     var operations=require('../database/helpers/Operation');
@@ -30,33 +30,33 @@ route.post('/createPoll',(req,res)=>{
 });
 
 // /getlistofpolls
-route.post('/getListofPolls',(req,res)=>{
+route.post('/getListofPolls',(req,res)=>{    //reqObject -->{}
     var reqObject=req.body;
 var operations=require('../database/helpers/Operation');
 operations.getlistofpolls(reqObject,res);
 });
 
-route.post('/deletePoll',(req,res)=>{
+route.post('/deletePoll',(req,res)=>{        //reqObject --> {u_id:"",poll_id:""}
     var reqObject=req.body;
 var operations=require('../database/helpers/Operation');
 operations.deletepoll(reqObject,res);
 });
 
-route.post('/findPoll',(req,res)=>{
+route.post('/findPoll',(req,res)=>{             //reqobject--> {poll_id:""}
     var reqObject=req.body;
 var operations=require('../database/helpers/Operation');
 operations.searchpoll(reqObject,res);
 });
 
 //  /vote
-route.post('/vote',(req,res)=>{
-    var reqObject=req.body;
+route.post('/vote',(req,res)=>{                  //reqObject -->{"u_id":"","p_id":"","o_id":""}
+    var reqObject=req.body; 
 var operations=require('../database/helpers/Operation');
 operations.vote(reqObject,res);
 });
 
 //myVotedPollsandSelectedOptions
-route.post('/myVotes',(req,res)=>{
+route.post('/myVotes',(req,res)=>{           //{"u_id":""}
     var reqObject=req.body;
 var operations=require('../database/helpers/Operation');
 operations.mypollsandselectedoption(reqObject,res);
@@ -67,7 +67,7 @@ operations.mypollsandselectedoption(reqObject,res);
 // /deleteprofile
 
 //  /mypolls
-route.post('/myPolls',(req,res)=>{
+route.post('/myPolls',(req,res)=>{            //reqObject -->{'u_id':""}
     var reqObject=req.body;
 var operations=require('../database/helpers/Operation');
 operations.mypolls(reqObject,res);
