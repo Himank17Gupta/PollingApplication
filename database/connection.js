738df0355af7efd689d657e7200c8d2200b53061
Himank17Gupta <hemankgupta@gmail.com>
2019-11-07 21:55:40 +0530

    added dotenv b4 live database

--- database/connection.js
+++ database/connection.js
@@ -1,3 +1,6 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/PollsMERN');            //dev
+require('dotenv').config();
+
+//mongoose.connect('mongodb://localhost:27017/PollsMERN');            //dev
+mongoose.connect(process.env.DB_KEY);            //prod
 module.exports=mongoose;
